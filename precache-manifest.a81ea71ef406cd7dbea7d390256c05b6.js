086bc4a0fae5cc4bf3e0389972aa54e634f79e65
Nacorga <nacho@MacBook-Pro.home>
2019-08-11 21:48:35 +0200

    Updates

---
+++ precache-manifest.a81ea71ef406cd7dbea7d390256c05b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0366d2b544a2cf796332d154b8beff",
+    "url": "/eventapp/index.html"
+  },
+  {
+    "revision": "cff8866f9a3cba106ca6",
+    "url": "/eventapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1b5371c01cec880a7747",
+    "url": "/eventapp/static/css/main.338c2677.chunk.css"
+  },
+  {
+    "revision": "cff8866f9a3cba106ca6",
+    "url": "/eventapp/static/js/2.3537407a.chunk.js"
+  },
+  {
+    "revision": "1b5371c01cec880a7747",
+    "url": "/eventapp/static/js/main.3c70e5c8.chunk.js"
+  },
+  {
+    "revision": "e1248b934425674fb38b",
+    "url": "/eventapp/static/js/runtime~main.85a39252.js"
+  },
+  {
+    "revision": "8e68c54d2d13f0b9d4053deb46f19c03",
+    "url": "/eventapp/static/media/logo.8e68c54d.svg"
+  }
+]);
